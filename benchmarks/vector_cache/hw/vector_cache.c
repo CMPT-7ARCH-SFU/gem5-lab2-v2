17019e84e4c2217364141bfe925e86bcec9fd35d
Milad Hakimi <miladhakimi33@gmail.com>
2022-09-04 19:25:46 -0700

    Update vector_cache

--- benchmarks/vector_cache/hw/vector_cache.c
+++ benchmarks/vector_cache/hw/vector_cache.c
@@ -1,10 +1,6 @@
 #include "vector.h"
 
 void vadd(TYPE m1[N], TYPE m2[N], TYPE prod[N] ){
-    // TYPE *m1 = (TYPE *)0x80c00000;
-    // TYPE *m2 = 0x80c00000 + sizeof(TYPE) * N;
-    // TYPE *prod = 0x80c00000 + sizeof(TYPE) * N * 2;
-    // m1[0] = m4[0];
     int i, j, k;
     int k_col, i_col;
     #pragma clang loop unroll_count(8)
